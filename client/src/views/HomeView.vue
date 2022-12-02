218a5462ad482abd7e22201a5bc4687278d07538
Brandon Apol <brandonapol@cedarville.edu>
2022-12-02 14:49:21 -0500

    preparing leaflet map for vue

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
+    
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import leaflet from "leaflet";
+import { onMounted } from "vue";
+
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
+  },
+  setup() {
+    let map;
+    onMounted(() => {
+      // make map
+      map = leaflet.map('map').setView([28.538336, -81.379234], 10);
+    })
   }
 }
 </script>
