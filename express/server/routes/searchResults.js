617c398e99ba04cf4069999d02514a3b0e20a1ec
Brandon Apol <brandonapol@cedarville.edu>
2022-12-02 11:54:40 -0500

    express app is working

---
+++ express/server/routes/searchResults.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const url = require("url");
+
+router.get('/:query', async (req, res) => {
+    try {
+        // add api key and query strings
+        const params = new URLSearchParams({
+            access_token: process.env.API_KEY,
+            ...url.parse(req.url, true).query,
+            // TODO: review url library of nodeJS
+        })
+        const query = req.params.query;
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`);
+        const data = results.data;
+        res.status(200).json(data);
+    } catch(err) {
+        res.status(500).json({ error: err.message });
+    };
+});
+
+module.exports = router
