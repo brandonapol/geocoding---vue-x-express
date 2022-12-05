e5e65e2d51afa26594231cdbd6560e3dfe9b8b0f
Brandon Apol <brandonapol@cedarville.edu>
2022-12-04 19:26:49 -0500

    full functionality

--- express/server/routes/searchResults.js
+++ express/server/routes/searchResults.js
@@ -13,6 +13,25 @@ router.get('/:query', async (req, res) => {
         })
         const query = req.params.query;
         const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`);
+
+        // format data for city and state
+        results.data.features.forEach((item) => {
+            // initiate to null
+            item.city = null;
+            item.state = null;
+
+            // loop through results
+            // context has an array of data like city, state etc 
+            item.context.forEach((type) => {
+                if (type.id.includes("place")) {
+                    item.city = type.text;
+                }
+                if (type.id.includes("region")) {
+                    item.state = type.text;
+                }
+            });
+        });
+
         const data = results.data;
         res.status(200).json(data);
     } catch(err) {
